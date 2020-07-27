3171cf718f40706227248cf07a80abfbd16e00b5
VentsislavAntov <55692897+VentsislavAntov@users.noreply.github.com>
2020-07-27 19:54:27 +0100

    customisedfiltering (#2)
    * fixed http request between database and react
    
    * Comments and slight refactor
    
    * Comments and slight refactor
    
    Co-authored-by: Ventsislav Antov <2504299a@student.gla.ac.uk>

--- server.js
+++ server.js
@@ -1,51 +1,75 @@
-const express = require('express')
-const app = express()
-const port = 3001
+/**
+ * This application is used to connect to the database and query its information upon API calls
+ * which is then used by the other application (react-iyengar-yoga-generator).
+ * The PostgreSQL tool used is pgAdmin
+ */
 
-const exercise_model = require('./exercise_model')
 
-app.use(express.json())
+//Note that the port used is 3001. This will prevent conflict with the other application which is on port 3000
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let pg = require('pg');
+const PORT = 3001;
+
+//Pools will use environment variables for connection information - the table on pgAdmin is called exercises.
+//Note that there is a maximum number of client connections set but this can be changed easily. Default should be 10 if not set
+let pool = new pg.Pool({
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    database: 'exercises',
+    max: 20
+});
+
+
+//Application framework for node. Morgan is used for logging request details
+let app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-// app.get('/', (req, res) => {
-//     res.status(200).send('Success');
-// })
+//Post
+app.post('/api/get-exercise', function (request, response) {
+    pool.connect((err, db, done) => {
+        if (err) {
+            return response.status(400).send(err);
+        } else {
+            db.query('SELECT * from exercise', (err, table) => {
+                done();
+                if (err) {
+                    return response.status(400).send(err);
+                } else {
+                    response.status(201).send(table.rows);
+                }
+            })
+        }
+    })
+});
 
-app.get('/', (req, res) => {
-    exercise_model.getExercises()
-        .then(response => {
-            res.status(200).send(response);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        })
-})
+//get
+app.get('/api/exercises', function (request, response) {
+    pool.connect(function (err, db, done) {
+        if (err) {
+            return response.status(400).send(err);
+        } else {
+            db.query('SELECT * from exercise', (err, table) => {
+                done();
+                if (err) {
+                    return response.status(400).send(err);
+                } else {
+                    response.status(200).send(table.rows);
+                }
+            })
+        }
+    })
+});
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
-
-
-// const {Client} = require('pg');
-// const client = new Client({
-//     user: 'postgres',
-//     password: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'exercises',
-//     max: 15
-// });
-//
-// function GetExercise(props) {
-//     client.connect()
-//         .then(() => console.log("Connected successfully"))
-//         .then(() => client.query("SELECT * from exercise WHERE englishname LIKE $1 AND sanskritname LIKE $2", props))
-//         .then(results => console.table(results.rows))
-//         .catch(e => console.log(e))
-//         .finally(() => client.end())
-// }
+//To display that it is listening
+app.listen(PORT, () => console.log('Listening on port: ' + PORT));
